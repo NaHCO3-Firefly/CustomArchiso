e4b6f3bf482d2390bc66f343313cd07f63a13af0
NaHCO3-Firefly <1226174871@qq.com>
2026-05-13 22:12:29 +0800

    speed up build: xz->zstd-3, parallel downloads 5->10

--- profiledef.sh
+++ profiledef.sh
@@ -11,8 +11,8 @@ buildmodes=('iso')
 bootmodes=('uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3' '-b' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '-3')
 file_permissions=(
   ["/etc/gshadow"]="0:0:0400"
   ["/etc/shadow"]="0:0:400"
